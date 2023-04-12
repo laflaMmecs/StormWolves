b27e6918a01a01424f6c7fab64e58f7065c50faa
laflaMme <maks.ponomarenko.2021@mail.ru>
2023-04-12 21:23:56 +0300

    header 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Storm Wolves Esports
-        </p>
-      </header>
+        <body>
+        <Header/>
+        </body>
     </div>
   );
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../img/logo.png';
+import ogneypor from '../img/ogneypor.png';
+import sobr from '../img/sobr.png'
+import classes from '../Header/Header.module.css';
+import News from "../News/News";
+const Header = () => {
+    return (
+        <div className={classes.header}>
+            <img src={logo} className={classes.img}>
+            </img>
+            <ul className={classes.nav}>
+                    <li><a href={<News />}>Новости</a></li>
+                    <li><a href={<News />}>Новости</a></li>
+                    <li><a href={<News />}>Новости</a></li>
+                    <li><a href={<News />}>Новости</a></li>
+            </ul>
+            <img src={ogneypor} className={classes.img_sponsor_og}></img>
+            <img src={sobr} className={classes.img_sponsor_sobr}></img>
+        </div>
+    );
+};
+
+export default Header;
